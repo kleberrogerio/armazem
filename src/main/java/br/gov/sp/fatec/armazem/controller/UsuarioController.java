d31ab41d2de7176b25a9af42c20a06ca0e6f2eab
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-25 13:43:05 +0000

    Usuario Controller

--- src/main/java/br/gov/sp/fatec/armazem/controller/UsuarioController.java
+++ src/main/java/br/gov/sp/fatec/armazem/controller/UsuarioController.java
@@ -1,18 +1,28 @@
 package br.gov.sp.fatec.armazem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//import br.gov.sp.fatec.armazem.service.SegurancaService;
-
+import br.gov.sp.fatec.armazem.entity.Usuario;
+import br.gov.sp.fatec.armazem.service.SegurancaService;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value="/usuario")
 public class UsuarioController{
 
-    //private SegurancaService segurancaService;
+    @Autowired
+    private SegurancaService segurancaService;
+
+    @GetMapping
+    public List<Usuario> buscarTodosUsuarios(){
+        return segurancaService.buscarTodosUsuarios();
+    }
 
 
 }
